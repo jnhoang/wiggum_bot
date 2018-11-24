5411fee18c8b071937fa1eab777c4a51b7c2b8af
jnhustin <jnhustin@gmail.com>
2018-11-24 13:01:10 -0800

    added handle for get quote

--- index.js
+++ index.js
@@ -2,37 +2,56 @@ require('dotenv').config()
 const SlackBot = require('slackbots');
 const axios    = require ('axios');
 
-const quotes = require('./ralph_quotes.json');
-
-console.log('1: ', quotes.length)
-
-
-const bot = new SlackBot({
-  token: process.env.SLACK_TOKEN,
-  name: 'ralph_wiggum'
-});
-
+const QUOTES    = require('./ralph_quotes.json');
 const EMOJI_OBJ = { icon_emoji: ':ralph_wiggum:' }
 
-// START HANDLER
-bot.on('start', () => bot.postMessageToChannel('test_channel', 'Hi!', EMOJI_OBJ) );
+console.log('1: ', QUOTES.length)
 
 
-// ERROR HANDLER
-bot.on('error', (error) => console.log(error))
+// const bot = new SlackBot({
+//   token: process.env.SLACK_TOKEN,
+//   name: 'ralph_wiggum'
+// });
 
 
-// MESSAGE HANDLER
-BOT.on('message', (data) => {
-  if (data.type !== 'message') {
-    return;
-  }
-  console.log('data:', data);
+// // START HANDLER
+// bot.on('start', () => bot.postMessageToChannel('test_channel', 'Hi!', EMOJI_OBJ) );
 
-  handleMessage(data.text);
-})
+
+// // ERROR HANDLER
+// bot.on('error', (error) => console.log(error))
+
+
+// // MESSAGE HANDLER
+// BOT.on('message', (data) => {
+//   if (data.type !== 'message') {
+//     return;
+//   }
+//   console.log('data:', data);
+
+//   handleMessage(data.text);
+// })
 
 
 function handleMessage(message) {
-
+  if (message.includes(' ralph quote')) {
+    getRandomQuote();
+  }
+  else if (message.includes(' ralph gif')) {
+    
+  }
 }
+
+
+const getRandomQuote = () => {
+  const qoutesLen = QUOTES.length;
+  const quote     = QUOTES[getRandomNum(qoutesLen)];
+  return quote;
+}
+
+
+const getRandomNum = (len) => Math.floor((Math.random() * len) + 1);
+
+
+
+console.log(getRandomQuote());
