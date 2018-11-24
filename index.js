fa5c8d8220ae3d22082d07e3dd045622587b7a2f
jnhustin <jnhustin@gmail.com>
2018-11-24 14:41:50 -0800

    added slack get_channel name function

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ require('dotenv').config()
 const SlackBot = require('slackbots');
 const axios    = require ('axios');
 
-const QUOTES    = require('./ralph_quotes.json');
-const EMOJI_OBJ = { icon_emoji: ':ralph_wiggum:' };
-const APP_USERNAME = process.env.APP_USERNAME;
+const QUOTES           = require('./ralph_quotes.json');
+const EMOJI_OBJ        = { icon_emoji: ':ralph_wiggum:' };
+const APP_USERNAME     = process.env.APP_USERNAME;
 const CREATOR_USERNAME = process.env.CREATOR_USERNAME;
-console.log('1: ', QUOTES.length)
 
 
 const bot = new SlackBot({
@@ -28,20 +27,32 @@ bot.on('message', (data) => {
   if (data.type !== 'message') {
     return;
   }
+  else if (data.subtype === 'message_deleted') {
+    return;
+  }
   
   console.log('data:', data);
-  handleMessage(data.text);
+
+  // if anything after handle message, need to add await keyword in front of handleMessage()
+  handleMessage(data);
 })
 
 
 
-function handleMessage(message) {
-  const keywords = ['ralph quote', APP_USERNAME, CREATOR_USERNAME];
+const handleMessage = async(data) => {
+  const message     = data.text;
+  const allChannels = await getChannelList();
+  const channelName     = allChannels.filter((channel) => channel.id === data.channel)[0].name;
+  console.log('name: ', channelName)
+  const keywords    = ['ralph quote', APP_USERNAME, CREATOR_USERNAME];
+  
   console.log('message: ', message)
+  console.log ('channel to post to: ', channel);
+  console.log ('channel name: ', channelName);
   
   if (hasMentionKeywords(message, keywords)) {
     quote = getRandomQuote();
-    bot.postMessageToChannel('test_channel', quote, EMOJI_OBJ)
+    bot.postMessageToChannel(channel, quote, EMOJI_OBJ)
   }
   // else if (message.includes(' ralph gif')) {
 
@@ -53,5 +64,11 @@ const getRandomNum = (len) => Math.floor((Math.random() * len) + 1);
 const getRandomQuote = () => QUOTES[getRandomNum(QUOTES.length)];
 const hasMentionKeywords = (msg, keywordsArr) => keywordsArr.some((keyword) => msg.includes(keyword));
 
+const getChannelList = async() => {
+  config = {
+    params: { token: process.env.SLACK_TOKEN, 'pretty': 1 } 
+  };
+  let res = await axios.get('https://slack.com/api/conversations.list', config);
+  return res.data.channels;
+}
 
-// console.log(getRandomQuote());
